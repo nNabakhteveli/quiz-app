b4073bc5b264212ef070e40fd427f43dcd2d1219
Nika <nabakhtevelin@gmail.com>
2021-05-02 17:08:16 +0400

    Adding credit to main page

--- components/showupQuiz.js
+++ components/showupQuiz.js
@@ -4,9 +4,13 @@ const question_text = document.getElementById('question-text');
 
 
 export default function styleHTML() {
+    // Make quiz appear
     quiz_container.style.visibility = 'visible';
-    document.getElementById('twitter-share').style.visibility = 'hidden';
     question_text.style.visibility = 'visible';
+
+    // Hide first page after starting the game
+    document.getElementById('twitter-share').style.visibility = 'hidden';
+    document.getElementById('credit').style.visibility = 'hidden';
     input_container.style.visibility = 'hidden';
     document.getElementById('endgame-div').style.visibility = 'hidden';
     document.getElementById('play-again-button').style.visibility = 'hidden';
